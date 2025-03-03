31dceaa8435459ef37a2e3a3f4a78c188fb5cd41
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2025-03-03 08:31:10 -0300

    Resolução XSS [Security https://github.com/LabRedesCefetRJ/WeGIA/security/advisories/GHSA-5x5w-5c99-vr8h]

--- html/funcionario/documento_listar.php
+++ html/funcionario/documento_listar.php
@@ -2,8 +2,9 @@
 <?php
 
 session_start();
-if (!isset($_SESSION["usuario"])){
+if (!isset($_SESSION["usuario"])) {
     header("Location: ../../index.php");
+    exit();
 }
 
 // Verifica Permissão do Usuário
@@ -11,12 +12,18 @@ require_once '../permissao/permissao.php';
 permissao($_SESSION['id_pessoa'], 11, 7);
 
 require_once "../../dao/Conexao.php";
-$pdo = Conexao::connect();
-$docfuncional = $pdo->query("SELECT * FROM funcionario_docfuncional ORDER BY nome_docfuncional ASC;");
-$docfuncional = $docfuncional->fetchAll(PDO::FETCH_ASSOC);
-$docfuncional = json_encode($docfuncional);
-echo $docfuncional;
+try {
+    $pdo = Conexao::connect();
+    $docfuncional = $pdo->query("SELECT * FROM funcionario_docfuncional ORDER BY nome_docfuncional ASC;");
+    $docfuncional = $docfuncional->fetchAll(PDO::FETCH_ASSOC);
 
-die();
+    foreach ($docfuncional as $index => $doc) {
+        $docfuncional[$index]['nome_docfuncional'] = htmlspecialchars($doc['nome_docfuncional']);
+    }
 
+    echo json_encode($docfuncional);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(['erro' => 'Erro ao buscar os tipos de documentos']);
+}
 ?>
