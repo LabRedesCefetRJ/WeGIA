dfe983f8b4db8c18ca33acd42f669a554409c9bf
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2025-03-20 07:44:27 -0300

    Alterada forma de acesso ao arquivo de config.php pelo método connect da classe de Conexao [Issue #559]

--- classes/Conexao.php
+++ classes/Conexao.php
@@ -1,16 +1,22 @@
 <?php
-if(file_exists("../config.php")){
-    require_once("../config.php");
-}else if(file_exists("config.php")){
-    require_once("config.php");
-}
+//requisição do arquivo de configuração
+require_once dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'config.php';
+
 class Conexao
 {
-    public static function connect()
+    /**
+     * Estabelece uma conexão PDO com o banco de dados da aplicação baseada nos dados contidos no arquivo de config.php,
+     * em caso de falha retorna false
+     */
+    public static function connect():PDO|false
     {
-        $pdo = new PDO('mysql:host='.DB_HOST.'; dbname='.DB_NAME.'; charset=utf8',DB_USER,DB_PASSWORD);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
+        try {
+            $pdo = new PDO('mysql:host=' . DB_HOST . '; dbname=' . DB_NAME . '; charset=utf8', DB_USER, DB_PASSWORD);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            //posteriormente fazer um sistema de armazenamento de log de erros
+            return false;
+        }
     }
 }
-
