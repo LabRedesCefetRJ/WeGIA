5a42a13898ad2bacc19b042f103097cc8fc51dec
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2025-04-11 08:57:34 -0300

    Implementação do método getLogsPorRecurso na classe SistemaLogDAO [Issue #947]

--- dao/SistemaLogDAO.php
+++ dao/SistemaLogDAO.php
@@ -19,7 +19,8 @@ class SistemaLogDAO
     /**
      * Realiza o registro de uma ação no sistema no banco de dados da aplicação
      */
-    public function registrar(SistemaLog $sistemaLog){
+    public function registrar(SistemaLog $sistemaLog)
+    {
         $sql = 'INSERT INTO sistema_log (id_pessoa, id_recurso, id_acao, descricao, data) VALUES (:idPessoa, :idRecurso, :idAcao, :descricao, :data)';
 
         $stmt = $this->pdo->prepare($sql);
@@ -31,5 +32,39 @@ class SistemaLogDAO
         $stmt->bindValue(':data', $sistemaLog->getData());
 
         return $stmt->execute();
+    }
+
+    /**
+     * Retorna o conjunto de logs do sistema armazenado no banco de dados de acordo com o recurso informado.
+     * @param $somenteUltimo TRUE para pegar o log com a data mais recente.
+     */
+    public function getLogsPorRecurso(int $idRecurso, bool $somenteUltimo = false):mixed
+    {
+        $sql = 'SELECT * FROM sistema_log  WHERE id_recurso=:idRecurso';
+
+        if ($somenteUltimo) {
+            $sql .= ' ORDER BY data DESC LIMIT 1';
+        } else {
+            $sql .= ' ORDER BY data DESC';
+        }
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindParam(':idRecurso', $idRecurso);
+
+        $stmt->execute();
+
+        if ($stmt->rowCount() < 1) {
+            return null;
+        }
+
+        $sistemaLogs = [];
+        $logsArray = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($logsArray as $log) {
+            $sistemaLog []= new SistemaLog($log['id_pessoa'], $log['id_recurso'], $log['id_acao'], $log['data'], $log['descricao']);
+        }
+
+        return $sistemaLogs;
     }
 }
