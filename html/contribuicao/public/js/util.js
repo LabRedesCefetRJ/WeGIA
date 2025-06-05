3653deeb640eb2282122bff14c13a3a287326322
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2025-06-05 12:22:20 -0300

    Consertado bug de autopreenchimento do campo Telefone [Issue #1049]

--- html/contribuicao/public/js/util.js
+++ html/contribuicao/public/js/util.js
@@ -304,10 +304,12 @@ async function cadastrarSocio() {
     const formData = new FormData(form);
 
     const documento = formatarCPF(pegarDocumento());
+    const telefone = formatarTelefone(formData.get('telefone'))
 
     formData.append('nomeClasse', 'SocioController');
     formData.append('metodo', 'criarSocio');
     formData.append('documento_socio', documento);
+    formData.append('telefone', telefone);
 
     try {
         const response = await fetch("../controller/control.php", {
@@ -575,4 +577,12 @@ function formatarCPF(cpf) {
   
     // Aplica a formatação: xxx.xxx.xxx-xx
     return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+  }
+
+  function formatarTelefone(telefone){
+    // Remove tudo que não for número
+    telefone = telefone.replace(/\D/g, '');
+  
+    // Aplica a formatação: (xx)xxxxx-xxxx
+    return telefone.replace(/(\d{2})(\d{5})(\d{4})/, "($1)$2-$3");
   }
