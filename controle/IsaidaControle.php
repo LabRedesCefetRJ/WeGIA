6e5ac30e057944ccb3c6a8692839a9c599153707
Joao Victor Pinheiro Grandini <joaovictorjvpg10@gmail.com>
2025-07-04 18:03:54 -0300

    issue[ #967] completa. FIz as alterações solicitadas pelo Nilson.

--- controle/IsaidaControle.php
+++ controle/IsaidaControle.php
@@ -1,6 +1,7 @@
 <?php
 include_once '../classes/Isaida.php';
 include_once '../dao/IsaidaDAO.php';
+include_once '../dao/SaidaDAO.php';
 
 class IsaidaControle
 {
@@ -9,8 +10,11 @@ class IsaidaControle
         try{
             $isaidaDAO = new IsaidaDAO();
             $isaida = $isaidaDAO->listarId($id_saida);
+            $saidaDAO = new SaidaDAO();
+            $saida = $saidaDAO->listarUmCompletoPorId($id_saida);
             session_start();
             $_SESSION['isaida'] = $isaida;
+            $_SESSION['saidaUnica'] = $saida;
             header('Location: ' . $nextPage);
         } catch (PDOException $e) {
             echo "ERROR: " . $e->getMessage();
