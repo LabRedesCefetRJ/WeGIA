0c8519987d29614ef9412ec9bcbe5c838d2f1435
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2025-07-07 12:11:07 -0300

    Resolução XSS em cadastro_pet.php [Security https://github.com/LabRedesCefetRJ/WeGIA/security/advisories/GHSA-mrwj-rf3q-3rqj]

--- dao/pet/adicionar_cor.php
+++ dao/pet/adicionar_cor.php
@@ -4,20 +4,16 @@ require_once '../../html/permissao/permissao.php';
 session_start();
 permissao($_SESSION['id_pessoa'], 6, 3);
 
-$entrada = trim($entrada);
-$entrada = htmlspecialchars($entrada, ENT_QUOTES, 'UTF-8');
-$entrada = strip_tags($entrada);
-
-function validarEntrada($entrada) {
-    if (!preg_match('/^[a-zA-Z\s-]+$/', $entrada)) {
-        throw new InvalidArgumentException("Caracteres inválidos na cor");
+function validarCor($cor) {
+    if (!preg_match('/^[a-zA-Z\s-]+$/', $cor)) {
+        throw new InvalidArgumentException("Caracteres inválidos na cor", 400);
     }
     return true;
 }
 
 try {
-    $cor = filter_input(INPUT_POST, 'cor', FILTER_SANITIZE_STRING);
-    validarEntrada($cor);
+    $cor = filter_input(INPUT_POST, 'cor', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    validarCor($cor);
     $pdo = Conexao::connect();
     $sql = "INSERT INTO pet_cor(descricao) VALUES (:cor)";
     $stmt = $pdo->prepare($sql);
@@ -26,9 +22,8 @@ try {
     
 } 
 
-catch (PDOException $e) {
-    // Erro de banco de dados
-    http_response_code(500);
-    echo json_encode(['erro' => 'Erro no servidor ao inserir a cor do pet.']);
+catch (Exception $e) {
+    http_response_code($e->getCode());
+    echo json_encode(['erro' => 'Erro ao inserir a cor do pet.']);
 } 
 ?>
