d63abd339a5d28c26530888dc7f82c6f4c3fe8af
Bruno-Buquer <brunobuquer@gmail.com>
2025-09-12 15:06:36 -0300

    Aplicação das correções de segurança em permissao.php

--- html/permissao/permissao.php
+++ html/permissao/permissao.php
@@ -1,5 +1,81 @@
 <?php
+function permissao($id_pessoa, $id_recurso, $id_acao = 1){
+	define("DEBUG", false);
+	//Xablau
 
+    require_once dirname(__FILE__, 3) . DIRECTORY_SEPARATOR . 'config.php';
+	/* 
+	$wegia_path = '';
+    $config_path = "config.php";
+	if(file_exists($wegia_path.$config_path)){
+		require_once($wegia_path.$config_path);
+	}else{
+        $cont = 0;
+		while($cont++ < 100){
+            $wegia_path .= "../";
+			if(file_exists($wegia_path.$config_path)) break;
+		}
+		require_once($wegia_path.$config_path);
+	}*/
+
+	require_once ROOT . '/dao/Conexao.php';
+
+	$pdo = Conexao::connect();
+
+	try { 
+		$sql = "SELECT * FROM funcionario WHERE id_pessoa = :ID_PESSOA";
+		$stmt = $pdo->prepare($sql);
+		$stmt->bindParam(':ID_PESSOA', $id_pessoa, PDO::PARAM_INT);
+		$stmt->execute();
+		$resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+	} catch(PDOException $e){ 
+		error_log('Erro: ' . $e->getMessage());
+	}
+
+	if (DEBUG){
+		// var_dump($resultado);
+		echo json_encode($resultado);
+		die();
+	}
+
+	if (!is_null($resultado)) {
+		$id_cargo = $resultado['id_cargo'];
+		try {
+			$sql = "SELECT * FROM permissao WHERE id_cargo = :id_cargo AND id_recurso = :id_recurso";
+			$stmt = $pdo->prepare($sql);
+			$stmt->bindValue(':id_cargo', $id_cargo, PDO::PARAM_INT);
+			$stmt->bindValue(':id_recurso', $id_recurso, PDO::PARAM_INT);
+			$stmt->execute();
+			$permissao = $stmt->fetch(PDO::FETCH_ASSOC);
+		} catch (PDOException $e) {
+			error_log("Erro no banco (permissao): " . $e->getMessage());
+			$permissao = false;
+		}
+		if ($permissao) {
+			if($permissao['id_acao'] < $id_acao){
+				$msg = "Você não tem as permissões necessárias para essa página.".(DEBUG ? " Sem acesso!" : "" );
+				header("Location: " . WWW . "html/home.php?msg_c=" . urlencode($msg));
+				//header("Location: ".$wegia_path."/html/home.php?msg_c=$msg");
+				exit();
+			}
+			$permissao = $permissao['id_acao'];
+		} else {
+			$permissao = $id_acao;
+			$msg = "Você não tem as permissões necessárias para essa página.".(DEBUG ? " Não há permissão!" : "" );
+			header("Location: " . WWW . "html/home.php?msg_c=" . urlencode($msg));
+			//header("Location: ".$wegia_path."/html/home.php?msg_c=$msg");
+			exit();
+		}
+	} else {
+		$permissao = $id_acao;
+		$msg = "Você não tem as permissões necessárias para essa página.".(DEBUG ? " Não há permissão!" : "" );
+		header("Location: " . WWW . "html/home.php?msg_c=" . urlencode($msg));
+		//header("Location: ".$wegia_path."/html/home.php?msg_c=$msg");
+		exit();
+	}
+}
+
+/*
 function permissao($id_pessoa, $id_recurso, $id_acao = 1){
 	define("DEBUG", false);
     $wegia_path = '';
@@ -48,4 +124,5 @@ function permissao($id_pessoa, $id_recurso, $id_acao = 1){
 		exit();
 	}
 }
+*/
 ?>
