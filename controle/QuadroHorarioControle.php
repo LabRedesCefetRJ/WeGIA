2febbaf47e55cc6729c92b95498f7b36ef3e93e5
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2025-09-30 16:47:05 -0300

    Resolução Open Redirect em QuadroHorarioControle [Security https://github.com/LabRedesCefetRJ/WeGIA/security/advisories/GHSA-wgv2-wqvm-jchf]

--- controle/QuadroHorarioControle.php
+++ controle/QuadroHorarioControle.php
@@ -7,15 +7,20 @@ class QuadroHorarioControle
 
     public function listarTipo()
     {
-        extract($_REQUEST);
+        require_once dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'config.php';
+        $nextPage = trim(filter_input(INPUT_GET, 'nextPage', FILTER_SANITIZE_URL));
+        $regex = '#^((\.\./|' . WWW . ')html/quadro_horario/(listar_tipo_quadro_horario)\.php)$#';
+
         (new QuadroHorarioDAO())->listarTipos();
-        header("Location: $nextPage");
+
+        preg_match($regex, $nextPage) ? header('Location:' . htmlspecialchars($nextPage)) : header('Location:' . '../html/home.php');
     }
 
     public function adicionarTipo()
     {
-        $tipo = trim(filter_input(INPUT_POST, 'tipo', FILTER_SANITIZE_STRING));
+        $tipo = trim(filter_input(INPUT_POST, 'tipo', FILTER_SANITIZE_SPECIAL_CHARS));
         $nextPage = trim(filter_input(INPUT_POST, 'nextPage', FILTER_SANITIZE_URL));
+        $regex = '#^((\.\./|' . WWW . ')html/quadro_horario/(adicionar_tipo_quadro_horario)\.php)$#';
 
         if (!$tipo || strlen($tipo) == 0) {
             http_response_code(400);
@@ -23,19 +28,22 @@ class QuadroHorarioControle
             exit();
         }
 
-        session_start();
+        if (session_status() === PHP_SESSION_NONE)
+            session_start();
+
         try {
-            $log = (new QuadroHorarioDAO())->adicionarTipo($tipo);;
+            $log = (new QuadroHorarioDAO())->adicionarTipo($tipo);
             $_SESSION['msg'] = $log;
         } catch (PDOException $e) {
-            echo ("Erro ao adicionar tipo '$tipo' ao banco de dados: " . $e->getMessage());
+            echo ('Erro ao adicionar tipo ' . htmlspecialchars($tipo) . ' ao banco de dados: ' . $e->getMessage());
             $_SESSION['msg'] = "Erro ao adicionar tipo: " . $e->getMessage();
             $_SESSION['flag'] = "erro";
         }
+
         $_SESSION['btnVoltar'] = true;
 
         if ($nextPage) {
-            header("Location: $nextPage");
+            preg_match($regex, $nextPage) ? header('Location:' . htmlspecialchars($nextPage)) : header('Location:' . '../html/home.php');
         }
     }
 
@@ -52,15 +60,20 @@ class QuadroHorarioControle
 
     public function listarEscala()
     {
-        extract($_REQUEST);
+        require_once dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'config.php';
+        $nextPage = trim(filter_input(INPUT_GET, 'nextPage', FILTER_SANITIZE_URL));
+        $regex = '#^((\.\./|' . WWW . ')html/quadro_horario/(listar_escala)\.php)$#';
+
         (new QuadroHorarioDAO())->listarEscalas();
-        header("Location: $nextPage");
+
+        preg_match($regex, $nextPage) ? header('Location:' . htmlspecialchars($nextPage)) : header('Location:' . '../html/home.php');
     }
 
     public function adicionarEscala()
     {
-        $escala = trim(filter_input(INPUT_POST, 'escala', FILTER_SANITIZE_STRING));
+        $escala = trim(filter_input(INPUT_POST, 'escala', FILTER_SANITIZE_SPECIAL_CHARS));
         $nextPage = trim(filter_input(INPUT_POST, 'nextPage', FILTER_SANITIZE_URL));
+        $regex = '#^((\.\./|' . WWW . ')html/quadro_horario/(adicionar_escala)\.php)$#';
 
         if (!$escala || strlen($escala) == 0) {
             http_response_code(400);
@@ -68,28 +81,44 @@ class QuadroHorarioControle
             exit();
         }
 
-        session_start();
+        if (session_status() === PHP_SESSION_NONE)
+            session_start();
+
         try {
             $log = (new QuadroHorarioDAO())->adicionarEscala($escala);
             $_SESSION['msg'] = $log;
         } catch (PDOException $e) {
-            echo ("Erro ao adicionar escala '$escala' ao banco de dados: " . $e->getMessage());
+            echo ('Erro ao adicionar escala ' . htmlspecialchars($escala) . ' ao banco de dados: ' . $e->getMessage());
             $_SESSION['msg'] = "Erro ao adicionar escala: " . $e->getMessage();
             $_SESSION['flag'] = "erro";
         }
         $_SESSION['btnVoltar'] = true;
 
         if ($nextPage) {
-            header("Location: $nextPage");
+            preg_match($regex, $nextPage) ? header('Location:' . htmlspecialchars($nextPage)) : header('Location:' . '../html/home.php');
         }
     }
 
     public function removerEscala()
     {
-        extract($_REQUEST);
+        $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+        $nextPage = trim(filter_input(INPUT_GET, 'nextPage', FILTER_SANITIZE_URL));
+        $regex = '#^((\.\./|' . WWW . ')html/quadro_horario/(listar_escala)\.php)$#';
+
+        if (!$id || $id < 1) {
+            http_response_code(400);
+            echo json_encode(['erro' => 'O id da escala fornecido é inválido.']);
+            exit();
+        }
+
         $log = (new QuadroHorarioDAO)->removerEscala($id);
-        session_start();
+
+        if (session_status() === PHP_SESSION_NONE)
+            session_start();
+
         $_SESSION['msg'] = $log;
-        header("Location: $nextPage");
+
+        preg_match($regex, $nextPage) ? header('Location:' . htmlspecialchars($nextPage)) : header('Location:' . '../html/home.php');
     }
 }
