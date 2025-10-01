cab477189df427b14b3526047213f2d9da383a03
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2025-10-01 10:39:09 -0300

    Resolução Open Redirect no método listarUm em PetControle [Security https://github.com/LabRedesCefetRJ/WeGIA/security/advisories/GHSA-wgv2-wqvm-jchf]

--- controle/pet/PetControle.php
+++ controle/pet/PetControle.php
@@ -1,98 +1,101 @@
 <?php
 $PetDAO_path = "dao/pet/PetDAO.php";
-if(file_exists($PetDAO_path)){
+if (file_exists($PetDAO_path)) {
     require_once($PetDAO_path);
-}else{
-    while(true){
+} else {
+    while (true) {
         $PetDAO_path = "../" . $PetDAO_path;
-        if(file_exists($PetDAO_path)) break;
+        if (file_exists($PetDAO_path)) break;
     }
     require_once($PetDAO_path);
 }
 //require_once '../../dao/pet/PetDAO.php';
 
 $Pet_path = "classes/pet/Pet.php";
-if(file_exists($Pet_path)){
+if (file_exists($Pet_path)) {
     require_once($Pet_path);
-}else{
-    while(true){
+} else {
+    while (true) {
         $Pet_path = "../" . $Pet_path;
-        if(file_exists($Pet_path)) break;
+        if (file_exists($Pet_path)) break;
     }
     require_once($Pet_path);
 }
 //require_once '../../classes/pet/Pet.php';
 
-class PetControle{
+class PetControle
+{
     private $petDAO;
     private $petClasse;
 
-    public function __construct(){
-       $this->petDAO = new PetDAO();
-       $this->petClasse = new PetClasse();
+    public function __construct()
+    {
+        $this->petDAO = new PetDAO();
+        $this->petClasse = new PetClasse();
     }
 
-    public function incluir() {
+    public function incluir()
+    {
         extract($_REQUEST);
-        
+
         $sexo = strtoupper($gender);
-    
+
         // Validações
         if (!isset($nome) || empty($nome)) {
             header("Location: ../../html/pet/cadastro_pet.php?msg=Nome não informado!");
             return;
         }
-    
+
         if (!isset($nascimento) || empty($nascimento)) {
             header("Location: ../../html/pet/cadastro_pet.php?msg=Data de nascimento não informada!");
             return;
         }
-    
+
         if (!isset($acolhimento) || empty($acolhimento)) {
             header("Location: ../../html/pet/cadastro_pet.php?msg=Data de acolhimento não informada!");
             return;
         }
-    
+
         if (!isset($sexo) || empty($sexo)) {
             header("Location: ../../html/pet/cadastro_pet.php?msg=Sexo não informado!");
             return;
         }
-    
+
         if (!isset($especie) || empty($especie)) {
             header("Location: ../../html/pet/cadastro_pet.php?msg=Espécie não informada!");
             return;
         }
-    
+
         if (!isset($raca) || empty($raca)) {
             header("Location: ../../html/pet/cadastro_pet.php?msg=Raça não informada!");
             return;
         }
-    
+
         if (!isset($cor) || empty($cor)) {
             header("Location: ../../html/pet/cadastro_pet.php?msg=Cor não informada!");
             return;
         }
-    
+
         if (!isset($caracEsp)) {
             $caracEsp = '';
         }
-    
+
         // Trata a imagem de perfil
         $imgperfil = '';
         $nomeImagem = ['', ''];
-    
+
         if (isset($_FILES['imgperfil']) && $_FILES['imgperfil']['error'] == UPLOAD_ERR_OK) {
             $tmpName = $_FILES['imgperfil']['tmp_name'];
             $imgperfil = base64_encode(file_get_contents($tmpName));
-    
+
             $nomeImagemCompleto = $_FILES['imgperfil']['name'];
             $nomeImagem = explode('.', $nomeImagemCompleto);
-    
+
             if (count($nomeImagem) < 2) {
                 $nomeImagem[1] = ''; // extensão vazia se não houver
             }
         }
-    
+
         // Define dados no objeto
         $this->petClasse->setNome($nome);
         $this->petClasse->setNascimento($nascimento);
@@ -104,7 +107,7 @@ class PetControle{
         $this->petClasse->setCor($cor);
         $this->petClasse->setImgPerfil($imgperfil);
         $this->petClasse->setNomeImagem($nomeImagem);
-    
+
         // Salva no banco
         $this->petDAO->adicionarPet(
             $this->petClasse->getNome(),
@@ -118,71 +121,79 @@ class PetControle{
             $this->petClasse->getImgPerfil(),
             $this->petClasse->getNomeImagem()
         );
-    
     }
-    
-    public function listarTodos(){
+
+    public function listarTodos()
+    {
         extract($_REQUEST);
-        $PetDAO= new PetDAO();
+        $PetDAO = new PetDAO();
         $pets = $PetDAO->listarTodos();
-        $_SESSION['pets']=$pets;
+        $_SESSION['pets'] = $pets;
         //header('Location: '.$nextPage);
     }
 
-    public function listarUmPet(){
+    public function listarUmPet()
+    {
         extract($_REQUEST);
         //try {
-            $petDAO = new PetDAO();
-            $pet=$petDAO->listarUm($idPet);
-            die(json_encode($pet));
+        $petDAO = new PetDAO();
+        $pet = $petDAO->listarUm($idPet);
+        die(json_encode($pet));
         //} catch (PDOException $e) {
         //    echo $e->getMessage();
         //}
     }
-    public function listarUm(){
-        extract($_REQUEST);
-        //try {
-            $petDAO = new PetDAO();
-            $pet=$petDAO->listarUm($id_pet);
-            if (session_status() === PHP_SESSION_NONE) {
-                session_start();
-            }
-            $_SESSION['pet']=$pet;
-            header('Location:'.$nextPage);
-        //} catch (PDOException $e) {
-        //    echo $e->getMessage();
-        //}
+    
+    public function listarUm()
+    {
+        $nextPage = trim(filter_input(INPUT_GET, 'nextPage', FILTER_SANITIZE_URL));
+        $regex = '#^((\.\./|' . WWW . ')html/pet/profile_pet\.php(\?id_pet=\d+)?)$#';
+
+        $idPet = filter_input(INPUT_GET, 'id_pet', FILTER_SANITIZE_NUMBER_INT);
+
+        $petDAO = new PetDAO();
+        $pet = $petDAO->listarUm($idPet);
+
+        if (session_status() === PHP_SESSION_NONE)
+            session_start();
+
+        $_SESSION['pet'] = $pet;
+
+        preg_match($regex, $nextPage) ? header('Location:' . htmlspecialchars($nextPage)) : header('Location:' . WWW . 'html/home.php');
     }
 
-    public function alterarImagem(){
+    public function alterarImagem()
+    {
         extract($_REQUEST);
         $imgPet = base64_encode(file_get_contents($_FILES['imgperfil']['tmp_name']));
         $imgNome = $_FILES['imgperfil']['name'];
-        $imgNome = explode('.', $imgNome);  
-        
-        try{
+        $imgNome = explode('.', $imgNome);
+
+        try {
             $petDAO = new PetDAO();
             $petDAO->alterarFotoPet($imgPet, $imgNome[0], $imgNome[1], $id_foto, $id_pet);
-            header('Location: ../../html/pet/profile_pet.php?id_pet='.$id_pet);            
-        }catch(PDOException $e){
+            header('Location: ../../html/pet/profile_pet.php?id_pet=' . $id_pet);
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
 
-    public function alterarPetDados(){
+    public function alterarPetDados()
+    {
         extract($_REQUEST);
 
         $sexo = strtoupper($gender);
-        try{
+        try {
             $petDAO = new PetDAO();
             $petDAO->alterarPet($nome, $nascimento, $acolhimento, $sexo, $especificas, $especie, $raca, $cor, $id_pet);
-            header('Location: ../../html/pet/profile_pet.php?id_pet='.$id_pet);   
-        }catch(PDOException $e){
+            header('Location: ../../html/pet/profile_pet.php?id_pet=' . $id_pet);
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
 
-    public function incluirExamePet(){
+    public function incluirExamePet()
+    {
         extract($_REQUEST);
         $nameFile = explode(".", $_FILES['arquivo']['name']);
         $arquivoExame = base64_encode(file_get_contents($_FILES['arquivo']['tmp_name']));
@@ -190,27 +201,26 @@ class PetControle{
         $dataExame = date("y-m-d");
         var_dump($_POST, $_FILES);
 
-        try{
+        try {
             $petDAO = new PetDAO();
-            $petDAO->incluirExamePet( $id_ficha_medica, $id_tipo_exame, $dataExame, $arquivoExame, $nameFile);
-            header("location: ../../html/pet/profile_pet.php?id_pet=".$id_pet);
-        }catch(PDOException $e){
+            $petDAO->incluirExamePet($id_ficha_medica, $id_tipo_exame, $dataExame, $arquivoExame, $nameFile);
+            header("location: ../../html/pet/profile_pet.php?id_pet=" . $id_pet);
+        } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
 
-    public function listarPets(){
-        
+    public function listarPets()
+    {
+
         header("Content-Type: application/json; charset=utf-8");
-        
+
         $petDao = new PetDAO();
 
-        
+
 
         $registros = $petDao->listarPets();
         http_response_code(200);
         echo json_encode($registros, JSON_UNESCAPED_UNICODE);
     }
 }
-
-?>
