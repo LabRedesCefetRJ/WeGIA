574f69214b31af7098421c41ef83a5d905bdaad3
Bruno-Buquer <brunobuquer@gmail.com>
2025-10-09 16:16:53 -0300

    Aplicação das mudanças sugeridas no Entrada.php - [#554]

--- classes/Entrada.php
+++ classes/Entrada.php
@@ -16,8 +16,18 @@ class Entrada
 
         $this->data=$data;
         $this->hora=$hora;
-        $this->valor_total=$valor_total;
-        $this->id_responsavel=$id_responsavel;
+        
+        // Valor total
+        if (!is_numeric($valor_total) || $valor_total < 0) {
+            throw new InvalidArgumentException("Valor total deve ser um número positivo.");
+        }
+        $this->valor_total = (float) $valor_total;
+
+        // ID do responsável
+        if (!is_numeric($id_responsavel) || $id_responsavel <= 0) {
+            throw new InvalidArgumentException("ID do responsável inválido.");
+        }
+        $this->id_responsavel = (int) $id_responsavel;
 
     }
 
@@ -63,6 +73,60 @@ class Entrada
 
     public function setId_entrada($id_entrada)
     {
+        if (!is_numeric($id_entrada) || $id_entrada <= 0)
+            throw new InvalidArgumentException("ID da entrada inválido.");
+        $this->id_entrada = (int) $id_entrada;
+    }
+
+    public function set_origem($id_origem)
+    {
+        if (!is_numeric($id_origem) || $id_origem <= 0)
+            throw new InvalidArgumentException("ID de origem inválido.");
+        $this->id_origem = (int) $id_origem;
+    }
+
+    public function set_almoxarifado($id_almoxarifado)
+    {
+        if (!is_numeric($id_almoxarifado) || $id_almoxarifado <= 0)
+            throw new InvalidArgumentException("ID do almoxarifado inválido.");
+        $this->id_almoxarifado = (int) $id_almoxarifado;
+    }
+
+    public function set_tipo($id_tipo)
+    {
+        if (!is_numeric($id_tipo) || $id_tipo <= 0)
+            throw new InvalidArgumentException("ID do tipo inválido.");
+        $this->id_tipo = (int) $id_tipo;
+    }
+
+    public function set_responsavel($id_responsavel)
+    {
+        if (!is_numeric($id_responsavel) || $id_responsavel <= 0)
+            throw new InvalidArgumentException("ID do responsável inválido.");
+        $this->id_responsavel = (int) $id_responsavel;
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    public function setHora($hora)
+    {
+        $this->hora = $hora;
+    }
+
+    public function setValor_total($valor_total)
+    {
+        if (!is_numeric($valor_total) || $valor_total < 0) {
+            throw new InvalidArgumentException("Valor total deve ser um número positivo.");
+        }
+        $this->valor_total = (float) $valor_total;
+    }
+
+    /*
+    public function setId_entrada($id_entrada)
+    {
         $this->id_entrada = $id_entrada;
     }
 
@@ -100,4 +164,5 @@ class Entrada
     {
         $this->valor_total = $valor_total;
     }
+    */
 }
