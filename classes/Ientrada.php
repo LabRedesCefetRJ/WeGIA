f406010a450a493ad0ed9010c90ecaee54cf63ed
Bruno-Buquer <brunobuquer@gmail.com>
2025-10-09 16:39:52 -0300

    Correções das sugestões do Ientrada.php - [#549]

--- classes/Ientrada.php
+++ classes/Ientrada.php
@@ -44,6 +44,39 @@ class Ientrada
         return $this->valor_unitario;
     }
 
+    public function setId_saida($id_saida)
+    {
+        if (!is_numeric($id_saida) || $id_saida <= 0) {
+            throw new InvalidArgumentException("ID de saída inválido.");
+        }
+        $this->id_saida = (int) $id_saida;
+    }
+
+    public function setId_produto($id_produto)
+    {
+        if (!is_numeric($id_produto) || $id_produto <= 0) {
+            throw new InvalidArgumentException("ID do produto inválido.");
+        }
+        $this->id_produto = (int) $id_produto;
+    }
+
+    public function setQtd($qtd)
+    {
+        if (!is_numeric($qtd) || $qtd <= 0) {
+            throw new InvalidArgumentException("A quantidade deve ser um número positivo.");
+        }
+        $this->qtd = (float) $qtd;
+    }
+
+    public function setValor_unitario($valor_unitario)
+    {
+        if (!is_numeric($valor_unitario) || $valor_unitario < 0) {
+            throw new InvalidArgumentException("O valor unitário deve ser um número não negativo.");
+        }
+        $this->valor_unitario = (float) $valor_unitario;
+    }
+    
+    /*
     public function setId_entrada($id_entrada)
     {
         $this->id_entrada = $id_entrada;
@@ -54,10 +87,10 @@ class Ientrada
         $this->id_produto = $id_produto;
     }
 
-    /*public function setData($data)
-    {
-        $this->data = $data;
-    }*/
+    // public function setData($data)
+    // {
+    //     $this->data = $data;
+    // }
 
     public function setQtd($qtd)
     {
@@ -68,4 +101,5 @@ class Ientrada
     {
         $this->valor_unitario = $valor_unitario;
     }
+    */
 }
