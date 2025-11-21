f2916b3338f528bdb76086fba6cd853d95302856
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2025-11-21 14:33:13 -0300

    Implementação do método incluir em ContatoInstituicaoMySQL [Issue #1271]

--- dao/ContatoInstituicaoMySQL.php
+++ dao/ContatoInstituicaoMySQL.php
@@ -16,7 +16,17 @@ class ContatoInstituicaoMySQL implements ContatoInstituicaoDAO{
 
     public function incluir(ContatoInstituicao $contato): int|false
     {
-        throw new \Exception('Not implemented');
+        $sql = 'INSERT INTO contato_instituicao(descricao, contato) VALUES (:descricao, :contato)';
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':descricao', $contato->getDescricao(), PDO::PARAM_STR);
+        $stmt->bindValue(':contato', $contato->getContato(), PDO::PARAM_STR);
+        $stmt->execute();
+
+        if($stmt->rowCount() < 1)
+            return false;
+
+        return intval($this->pdo->lastInsertId());
     }
 
     public function listarPorId(int $id): ?ContatoInstituicao
