742d6f737b49b23de688abfa9a482a1a33b070e7
GabrielPintoSouza <gabrielpintosouz4@gmail.com>
2026-01-23 11:03:23 -0300

    create: Implementação do método create de PessoaArquivoMySQL [Issue #1344]

--- dao/PessoaArquivoDAO.php
+++ dao/PessoaArquivoDAO.php
@@ -2,6 +2,7 @@
 require_once dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'PessoaArquivoDTO.php';
 
 interface PessoaArquivoDAO{
+    public function __construct(?PDO $pdo = null);
     public function create(PessoaArquivo $pessoaArquivo):int|false;
     public function delete(int $id):bool;
     public function getById(int $id):PessoaArquivoDTO;
--- dao/PessoaArquivoMySQL.php
+++ dao/PessoaArquivoMySQL.php
@@ -1,11 +1,34 @@
 <?php
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Conexao.php';
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PessoaArquivoDAO.php';
 require_once dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . 'PessoaArquivoDTO.php';
 
-class PessoaArquivoMySQL implements PessoaArquivoDAO{
+class PessoaArquivoMySQL implements PessoaArquivoDAO
+{
+    private PDO $pdo;
+
+    public function __construct(?PDO $pdo = null)
+    {
+        isset($pdo) ? $this->pdo = $pdo : $this->pdo = Conexao::connect();
+    }
+
     public function create(PessoaArquivo $pessoaArquivo): int|false
     {
-        throw new \Exception('Not implemented');
+        $query = 'INSERT INTO 
+                    pessoa_arquivo (id_pessoa, arquivo_nome, arquivo_extensao, arquivo) 
+                    VALUES (:idPessoa, :arquivoNome, :arquivoExtensao, :arquivo)
+                ';
+
+        $stmt = $this->pdo->prepare($query);
+        $stmt->bindValue(':idPessoa', $pessoaArquivo->getIdPessoa(), PDO::PARAM_INT);
+        $stmt->bindValue(':arquivoNome', $pessoaArquivo->getArquivo()->getNome(), PDO::PARAM_STR);
+        $stmt->bindValue(':arquivoExtensao', $pessoaArquivo->getArquivo()->getExtensao(), PDO::PARAM_STR);
+        $stmt->bindValue(':arquivo', $pessoaArquivo->getArquivo()->getConteudo(), PDO::PARAM_LOB);
+
+        if(!$stmt->execute())
+            return false;
+
+        return (int) $this->pdo->lastInsertId();
     }
 
     public function delete(int $id): bool
