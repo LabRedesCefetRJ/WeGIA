37242a6930c6a058f3e7ccaf8b1ea81dcec84fe2
Pedro Schuenck <pschuenckbittencourt@gmail.com>
2026-03-19 10:03:50 -0300

    fix: normalização da biblioteca da tabela [issue #1442]

--- assets/javascripts/tables/examples.datatables.default.js
+++ assets/javascripts/tables/examples.datatables.default.js
@@ -5,38 +5,13 @@ Theme Version: 	1.3.0
 */
 
 (function( $ ) {
+
 	'use strict';
 
-	var removeAccents = function(value) {
-		if (value === null || value === undefined) {
-			return '';
-		}
-
-		var text = String(value).toLowerCase();
-
-		if (typeof text.normalize === 'function') {
-			return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-		}
-
-		return text;
-	};
-
-	if ($.fn.dataTable && $.fn.dataTable.ext && $.fn.dataTable.ext.type && $.fn.dataTable.ext.type.search) {
-		$.fn.dataTable.ext.type.search.string = function(data) {
-			return removeAccents(data);
-		};
-
-		$.fn.dataTable.ext.type.search.html = function(data) {
-			return removeAccents(data);
-		};
-	}
-
 	var datatableInit = function() {
-		var $table = $('#datatable-default');
 
-		if ($table.length && !$.fn.DataTable.isDataTable($table)) {
-			$table.dataTable();
-		}
+		$('#datatable-default').dataTable();
+
 	};
 
 	$(function() {
