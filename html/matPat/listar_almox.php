29e292ea10cc163e4d896bf32a0664ceb61f7d66
nicolly015 <nicollyboy01@gmail.com>
2026-05-04 10:36:07 -0300

    fix: bug visual no botão de ativos/arquivados [Issue #1593]

--- html/matPat/listar_almox.php
+++ html/matPat/listar_almox.php
@@ -165,16 +165,14 @@ require_once ROOT . "/html/personalizacao_display.php";
 					</header>
 					<div class="panel-body">
 						<div style="margin-bottom: 15px;">
-							<a href="listar_almox.php?tipo=ativo">
-								<button <?= $tipo === 'ativo' ? 'style="font-weight:bold;"' : ''?>>
-									Ativos
-								</button>
+							<a href="listar_almox.php?tipo=ativo"
+								class="btn btn-default <?= $tipo === 'ativo' ? 'active' : ''?>">
+								Ativos
 							</a>
 
-							<a href="listar_almox.php?tipo=arquivado">
-								<button <?= $tipo === 'arquivado' ? 'style="font-weight:bold;"' : ''?>>
-									Arquivados
-								</button>
+							<a href="listar_almox.php?tipo=arquivado"
+								class="btn btn-default <?= $tipo === 'arquivado' ? 'active' : ''?>">
+								Arquivados
 							</a>
 						</div>
 						<?php if(isset($_SESSION['erro'])): ?>
