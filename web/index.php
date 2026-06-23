98c2b377062f96756172d4c05c3b78df095304ed
João Victor Pinheiro Grandini <joaovictorjvpg10@gmail.com>
2026-06-23 16:34:43 -0300

    [#1730]

--- web/index.php
+++ web/index.php
@@ -15,9 +15,8 @@ require_once "html/personalizacao_display.php";
 <head>
 	<title><?php display_campo("Titulo", "str"); ?> - <?php display_campo("Subtitulo", "str"); ?></title>
 	<meta charset="UTF-8" />
-	<link rel="icon" href="<?php display_campo("Logo", "file"); ?>" type="image/x-icon">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="icon" href="<?php display_campo("Logo", "file"); ?>" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<!-- Web Fonts  -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
 	<!-- font inter -->
