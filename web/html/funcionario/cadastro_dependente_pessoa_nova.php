7b22de9d6311442881c8d971aed65d2fb6090390
João Victor Pinheiro Grandini <joaovictorjvpg10@gmail.com>
2026-06-23 17:58:25 -0300

    [#1754]

--- web/html/funcionario/cadastro_dependente_pessoa_nova.php
+++ web/html/funcionario/cadastro_dependente_pessoa_nova.php
@@ -579,7 +579,7 @@ $fieldErrors = getSessionFormErrors();
   <script src="../../assets/vendor/jquery-placeholder/jquery.placeholder.js"></script>
 
   <div align="right">
-    <iframe src="https://www.wegia.org/software/footer/pessoa.html" width="200" height="60" style="border:none;"></iframe>
+    <iframe src="https://www.wegia.org/software/footer/pessoa.html" width="200" height="60" style="border:none;" title="Rodapé do WeGIA"></iframe>
   </div>
 </body>
 
