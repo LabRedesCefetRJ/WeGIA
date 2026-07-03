91d588b45da48187c59f78943660540f4bd8289d
Enzo Darcy <pacacumflux@gmail.com>
2026-07-03 17:35:46 -0300

    #1909 modificando Voluntario.php de acordo com o comentário na pull request 1909

--- web/classes/Voluntario.php
+++ web/classes/Voluntario.php
@@ -36,26 +36,41 @@ class Voluntario extends Pessoa
 
     public function setId_voluntario($id_voluntario)
     {
+        if (!is_numeric($id_voluntario) || $id_voluntario < 0) {
+            throw new InvalidArgumentException("O id deve ser um valor numérico não negativo.", 412);
+        }
         $this->id_voluntario = $id_voluntario;
     }
 
     public function setId_pessoa($id_pessoa)
     {
+        if (!is_numeric($id_pessoa) || $id_pessoa < 0) {
+            throw new InvalidArgumentException("O id deve ser um valor numérico não negativo.", 412);
+        }
         $this->id_pessoa = $id_pessoa;
     }
 
     public function setId_situacao($id_situacao)
     {
+        if (!is_numeric($id_situacao) || $id_situacao < 0) {
+            throw new InvalidArgumentException("O id deve ser um valor numérico não negativo.", 412);
+        }
         $this->id_situacao = $id_situacao;
     }
 
     public function setId_cargo($id_cargo)
     {
+        if (!is_numeric($id_cargo) || $id_cargo < 0) {
+            throw new InvalidArgumentException("O id deve ser um valor numérico não negativo.", 412);
+        }
         $this->id_cargo = $id_cargo;
     }
 
-    public function setData_admissao($data_admissao)
+    public function setData_admissao(date $data_admissao)
     {
+        if ($data_admissao > new DateTime()) {
+            throw new InvalidArgumentException("A data não pode ser futura.", 412);
+        }
         $this->data_admissao = $data_admissao;
     }
 
