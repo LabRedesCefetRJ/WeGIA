d4cda5f4be2be55ce1bd1ed9d81c51ce6e173d4e
KayllanBSanches <21198405716@cefet-rj.br>
2026-09-01 10:37:56 -0300

    Fix: correção de erro por alteração feita no banco de dados para realização da issue issue #1672 em profile_paciente [issue #2022]

--- web/html/saude/profile_paciente.php
+++ web/html/saude/profile_paciente.php
@@ -161,14 +161,14 @@ $descricao_medica = json_encode($descricao_medica);
 $stmtMedicacoes = $pdo->prepare("
     SELECT sm.id_medicacao, sa.data_atendimento, sm.medicamento, sm.dosagem,
            GROUP_CONCAT(TIME_FORMAT(smh.horario, '%H:%i') ORDER BY smh.horario SEPARATOR ', ') AS horarios,
-           sm.duracao, st.descricao,
+           sm.tipo_de_uso, st.descricao,
            sm.saude_medicacao_status_idsaude_medicacao_status AS id_status
     FROM saude_atendimento sa
     JOIN saude_medicacao sm ON (sa.id_atendimento = sm.id_atendimento)
     JOIN saude_medicacao_status st ON (sm.saude_medicacao_status_idsaude_medicacao_status = st.idsaude_medicacao_status)
     LEFT JOIN saude_medicacao_horario smh ON (smh.id_medicacao = sm.id_medicacao)
     WHERE sa.id_fichamedica = :idFichaMedica
-    GROUP BY sm.id_medicacao, sa.data_atendimento, sm.medicamento, sm.dosagem, sm.duracao, st.descricao, sm.saude_medicacao_status_idsaude_medicacao_status
+    GROUP BY sm.id_medicacao, sa.data_atendimento, sm.medicamento, sm.dosagem, sm.tipo_de_uso, st.descricao, sm.saude_medicacao_status_idsaude_medicacao_status
     ORDER BY sa.data_atendimento DESC, sm.id_medicacao DESC
 ");
 
